995f072601213fed28fc29a7c6c1053499c69ae3
braun07 <joaovitorbraun6@gmail.com>
2026-08-31 11:44:25 -0300

    feat: create functions to populate classes + displays, justify, radius + spacing + code organization

--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -1,19 +1,18 @@
 import { useState } from 'react'
 import heroImg from '../assets/bramble.svg'
 import {Button} from '../components/index.ts'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
+      <section id="center" className="flex flex-col items-center justify-center m-auto">
         <img src={heroImg} className="base" width="170" height="179" alt="" />
 
         <Button
           type="button"
-          className="counter"
+          className="flex mt-10 rounded-1"
           onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
